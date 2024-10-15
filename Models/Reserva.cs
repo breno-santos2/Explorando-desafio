082303dbe9419a50ca2641f6eb8d65490afa4e5d
Breno Santos <breno.dev27@gmail.com>
2024-10-15 13:26:18 -0300

    Implementação

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,4 +1,4 @@
-namespace DesafioProjetoHospedagem.Models
+/*namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
     {
@@ -51,3 +51,4 @@ namespace DesafioProjetoHospedagem.Models
         }
     }
 }
+*/
