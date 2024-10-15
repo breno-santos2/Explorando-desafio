34bd313b31bd8162e38dc445b589aff319c04f50
Breno Santos <breno.dev27@gmail.com>
2024-10-15 12:02:32 -0300

    Explorando

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -2,20 +2,28 @@ namespace DesafioProjetoHospedagem.Models;
 
 public class Pessoa
 {
+    //Construtor padrão
     public Pessoa() { }
 
+    // Construtor que recebe apenas o nome
     public Pessoa(string nome)
     {
         Nome = nome;
     }
 
+    // Construtor que recebe nome e sobrenome
     public Pessoa(string nome, string sobrenome)
     {
         Nome = nome;
         Sobrenome = sobrenome;
     }
 
+    // Propriedade para armazenar o nome
     public string Nome { get; set; }
+
+    // Propriedade para armazenar o sobrenome
     public string Sobrenome { get; set; }
+
+    // Propriedade que retorna o nome completo em letras maiúsculas
     public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
 }
--- Models/Suite.cs
+++ Models/Suite.cs
@@ -1,9 +1,11 @@
 namespace DesafioProjetoHospedagem.Models
 {
+    // Construtor padrão
     public class Suite
     {
         public Suite() { }
 
+        // Construtor que recebe tipo de suíte, capacidade e valor da diária
         public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
         {
             TipoSuite = tipoSuite;
@@ -11,8 +13,11 @@ namespace DesafioProjetoHospedagem.Models
             ValorDiaria = valorDiaria;
         }
 
+        // Propriedade que representa o tipo da suíte
         public string TipoSuite { get; set; }
+        // Propriedade que representa a capacidade máxima da suíte
         public int Capacidade { get; set; }
+        // Propriedade que representa o valor da diária da suíte
         public decimal ValorDiaria { get; set; }
     }
 }
